496f2a78ea6e74f039e3570a51c0ae1658972766
jvf96 <31595919+jvf96@users.noreply.github.com>
2017-11-12 23:36:01 +0100

    Update greedy_knapsack.R

--- LAB6/R/greedy_knapsack.R
+++ LAB6/R/greedy_knapsack.R
@@ -7,7 +7,9 @@
 #' @examples
 #' set.seed(42) 
 #' n <- 2000 
-#' knapsack_objects <- data.frame( w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000))
+#' knapsack_objects <- data.frame(
+#' w=sample(1:4000, size = n, replace = TRUE),
+#' v=runif(n = n, 0, 10000))
 #' greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
 #' greedy_knapsack(x = knapsack_objects[1:1200,], W = 2000)
 #' 
